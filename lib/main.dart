42b406bae320540b7e925597b5b732808ed78a20
talalhabib <talalhabib0000@outlook.com>
2024-02-05 18:58:11 +0500

    login screen ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:socialmedia/responsive/mobile_screen_layout.dart';
+import 'package:socialmedia/responsive/responsive_layout_screen.dart';
+import 'package:socialmedia/responsive/web_screen_layout.dart';
+import 'package:socialmedia/screens/login_screen.dart';
+import 'package:socialmedia/utils/colors.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -16,11 +21,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Text('Flutter Demo Home Page'),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData.dark()
+            .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
+        // home: const ResponsiveLayout(
+        //   mobileScreenLayout: MobileScreenLayout(),
+        //   webScreenLayout: WebScreenLayout(),
+        // ),
+        home: const LoginScreen());
   }
 }
